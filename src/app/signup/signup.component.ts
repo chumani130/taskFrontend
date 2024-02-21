4b4c03b763ed0446d49567bc687170cb76497571
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-02-21 14:54:03 +0200

    Update signup.component.ts

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { confirmPasswordValidator } from '../validators/confirm-password-validator';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss']
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit{
+  
 
+  // fb = inject(FormBuilder);
+  constructor(private fb: FormBuilder) { }
+  registerForm !: FormGroup
+  
+  ngOnInit(): void {
+    // throw new Error('Method not implemented.');
+    this.registerForm = this.fb.group({
+      userName: ['', Validators.required],
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required],
+      subscribe: [false] // unchecked by default
+    },
+    
+    {
+      validator: confirmPasswordValidator('password', 'confirmPassword')
+    }
+    );
+    }
+    register() {
+      console.log(this.registerForm.value);
+    }
+    signUpWithGoogle() {
+      console.log("you have signed up with google")
+    }
 }
