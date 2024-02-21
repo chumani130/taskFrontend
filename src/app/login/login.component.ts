219693ae59df3ef730232c3d930e02e7d8558e24
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-02-21 14:30:09 +0200

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
+  constructor(private fb: FormBuilder) { }
+  loginForm !: FormGroup
+
+  ngOnInit(): void {
+    // throw new Error('Method not implemented.');
+    this.loginForm = this.fb.group({
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      password: ['', Validators.required]
+    }    
+    );
+    }
+    login() {
+      console.log(this.loginForm.value);
+    }
+    signInWithGoogle() {
+      console.log("you have signed up with google")
+    }
 }
