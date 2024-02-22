7ce215830fa2e7f763cefbc2d68f57ca2f23d4bd
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-02-22 10:56:41 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { AboutComponent } from './about/about.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 const routes: Routes = [
   { path: '', component: LandingPageComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
-  { path: 'about', component: AboutComponent}
+  { path: 'about', component: AboutComponent},
+  { path: 'forgot-password', component: ForgotPasswordComponent}
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
