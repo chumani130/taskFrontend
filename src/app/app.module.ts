250c84c38cf8f8a4a2b5da26c823c9e594bbff25
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-02-22 10:57:01 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { AboutComponent } from './about/about.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { AboutComponent } from './about/about.component';
     LandingPageComponent,
     LoginComponent,
     SignupComponent,
-    AboutComponent
+    AboutComponent,
+    ForgotPasswordComponent
   ],
   imports: [
     BrowserModule,
