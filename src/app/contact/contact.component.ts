fd3abee7222634accf156a347c1abb2915551ae4
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-02-22 13:05:51 +0200

    Update contact.component.ts

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
 
+  contactForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      subject: ['', Validators.required],
+      message: ['', Validators.required]
+    });
+  }
+
+  submitForm() {
+    if (this.contactForm.valid) {
+      // Form is valid, you can proceed with submitting the data
+      console.log(this.contactForm.value);
+    } else {
+      // Form is invalid, handle accordingly (show error messages, etc.)
+      console.log("Invalid form");
+    }
+  }
 }
