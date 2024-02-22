02dd8f945cddd862c1026c95e1eea324fb3cc771
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-02-22 11:18:39 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,16 @@ import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { AboutComponent } from './about/about.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
-  { path: '', component: LandingPageComponent },
+  {path: '', redirectTo: '/landingpage', pathMatch: 'full'},
+  { path: 'landingpage', component: LandingPageComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'about', component: AboutComponent},
-  { path: 'forgot-password', component: ForgotPasswordComponent}
+  { path: 'forgot-password', title: 'Forgot Password Page', component: ForgotPasswordComponent},
+  {path: '**', title: 'Error: Page Not Found', component: NotFoundComponent},
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
