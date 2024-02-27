d1a4bcf369003249dd4753b1ea2b41dda4871943
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-02-27 13:52:05 +0200

    Update signup.component.ts

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,17 +1,18 @@
 import { Component, OnInit, inject } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
 import { confirmPasswordValidator } from '../validators/confirm-password-validator';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss']
 })
-export class SignupComponent implements OnInit{
-  
+export class SignupComponent implements OnInit{  
 
   // fb = inject(FormBuilder);
   constructor(private fb: FormBuilder) { }
   registerForm !: FormGroup
+  hidePassword = true;
+  hideConfirmPassword = true;
   
   ngOnInit(): void {
     // throw new Error('Method not implemented.');
@@ -19,7 +20,7 @@ export class SignupComponent implements OnInit{
       userName: ['', Validators.required],
       email: ['', Validators.compose([Validators.required, Validators.email])],
       password: ['', Validators.required],
-      confirmPassword: ['', Validators.required],
+      confirmPassword: ['', [Validators.required, this.passwordMatchValidator]],
       subscribe: [false] // unchecked by default
     },
     
@@ -34,4 +35,21 @@ export class SignupComponent implements OnInit{
     signUpWithGoogle() {
       console.log("you have signed up with google")
     }
+    togglePasswordVisibility(): void {
+      this.hidePassword = !this.hidePassword;
+    }
+    toggleConfirmPasswordVisibility(): void {
+      this.hideConfirmPassword = !this.hideConfirmPassword;
+    }
+    passwordMatchValidator(control: AbstractControl): { [key: string]: boolean } | null {
+      const password = control.get('password');
+      const confirmPassword = control.get('confirmPassword');
+  
+      if (password && confirmPassword && password.value !== confirmPassword.value) {
+          return { 'confirmPasswordValidator': true };
+      }
+  
+      return null;
+  }
+  
 }
