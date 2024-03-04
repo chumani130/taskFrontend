4dbf2cc93d4911da125a7c7f3d9f0268b0ebd305
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-03-04 11:18:58 +0200

    Update user-storage-service.service.ts

--- src/app/services/storage/user-storage-service.service.ts
+++ src/app/services/storage/user-storage-service.service.ts
@@ -16,6 +16,6 @@ export class UserStorageService {
   }
   public saveUser(user): void {
     window.localStorage.removeItem(USER);
-    window.localStorage.setItem(TOKEN, JSON.stringify(user));
+    window.localStorage.setItem(USER, JSON.stringify(user));
   }
 }
