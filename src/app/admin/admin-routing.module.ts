76577eb9141fb522b7eac6a0ddde368c4f55ce50
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-03-06 13:32:31 +0200

    Update admin-routing.module.ts

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -4,12 +4,24 @@ import { AdminComponent } from './admin.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { NotificationsComponent } from './notifications/notifications.component';
+import { SettingsComponent } from './settings/settings.component';
+import { SearchComponent } from './search/search.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { ChatsComponent } from './chats/chats.component';
+import { CustomerComponent } from '../customer/customer.component';
 
 const routes: Routes = [
   { path: '', component: AdminComponent }, 
   { path: 'topbar', component: TopBarComponent},
   { path: 'sidebar', component: SidebarComponent },
-  { path: 'dashboard', component: DashboardComponent }
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'notifications', component: NotificationsComponent },
+  { path: 'settings', component: SettingsComponent },
+  { path: 'search', component: SearchComponent },
+  { path: 'tasks', component: TasksComponent },
+  { path: 'chats', component: ChatsComponent },
+  { path: 'customers', component: CustomerComponent }
 ];
 
 @NgModule({
