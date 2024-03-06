e58424389fa7c84f4c158fc0ce1e532d3507a7c9
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-03-06 09:51:52 +0200

    Update admin-routing.module.ts

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [{ path: '', component: AdminComponent }];
+const routes: Routes = [
+  { path: '', component: AdminComponent }, 
+  { path: 'topbar', component: TopBarComponent},
+  { path: 'sidebar', component: SidebarComponent },
+  { path: 'dashboard', component: DashboardComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
