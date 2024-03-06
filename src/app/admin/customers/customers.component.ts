a9e9e5c4d4e29f781ba4e9b508b16c6877e365ee
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-03-06 15:12:33 +0200

    Update customers.component.ts

--- src/app/admin/customers/customers.component.ts
+++ src/app/admin/customers/customers.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-users',
+  selector: 'app-customers',
   templateUrl: './customers.component.html',
   styleUrls: ['./customers.component.scss']
 })
